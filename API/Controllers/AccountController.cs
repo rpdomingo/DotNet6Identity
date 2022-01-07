a0db571bb87b9ecd70db52598bfce63cfd970aa2
reynbow domingo <reynbow_domingo@outlook.com>
2022-01-07 14:54:11 +0800

    Add Account Controller, Dto and IdentityExtensions

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+using API.Dtos;
+using API.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest("Failed to register");
+
+            return Ok();
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Entities.Identity;
+using API.Extensions;
 using API.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -18,7 +21,6 @@ namespace API
 {
     public class Startup
     {
-
         public IConfiguration _config { get; }
         
         public Startup(IConfiguration configuration)
@@ -37,6 +39,8 @@ namespace API
             services.AddDbContext<AppIdentityDbContext>(x => {
                 x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
             });
+
+            services.AddIdentityServices(_config);
 
             services.AddSwaggerGen(c =>
             {
